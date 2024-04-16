b2fbd53ba02a971177e03ce23d3d43eb203f92df
pimenteljoao44 <joaovitordasilvapimentel0@gmail.com>
2024-04-16 09:33:30 -0300

    variaveis de ambiente prod

--- src/main/java/com/joao/osMarmoraria/OsMarmorariaApplication.java
+++ src/main/java/com/joao/osMarmoraria/OsMarmorariaApplication.java
@@ -12,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OsMarmorariaApplication {
 	
 	public static void main(String[] args) {
+
+		System.out.println("JDBC_DATABASE_URL: " + System.getenv("DATABASE_URL"));
+		System.out.println("JDBC_DATABASE_USERNAME: " + System.getenv("JDBC_DATABASE_USERNAME"));
+		System.out.println("JDBC_DATABASE_PASSWORD: " + System.getenv("JDBC_DATABASE_PASSWORD"));
 		SpringApplication.run( OsMarmorariaApplication.class,args);
 	}
 }
