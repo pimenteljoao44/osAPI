e79712c3520d317bbd784f8e4b8470f84884185f
João Vitor Pimentel <100111853+pimenteljoao44@users.noreply.github.com>
2024-07-15 23:15:42 -0300

    Update SecurityConfig.java

--- src/main/java/com/joao/osMarmoraria/security/SecurityConfig.java
+++ src/main/java/com/joao/osMarmoraria/security/SecurityConfig.java
@@ -2,15 +2,24 @@ package com.joao.osMarmoraria.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,4 +33,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
