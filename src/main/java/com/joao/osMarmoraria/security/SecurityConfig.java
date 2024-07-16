d153eecd7ab71b89fc50bf891780d40cfaca9eed
João Vitor Pimentel <100111853+pimenteljoao44@users.noreply.github.com>
2024-07-15 23:00:06 -0300

    Update SecurityConfig.java
    permitindo qualquer requisição

--- src/main/java/com/joao/osMarmoraria/security/SecurityConfig.java
+++ src/main/java/com/joao/osMarmoraria/security/SecurityConfig.java
@@ -1,19 +1,11 @@
 package com.joao.osMarmoraria.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,46 +16,16 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    SecurityFilter securityFilter;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .cors().and()
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests(authorize -> authorize
-                        .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .antMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .antMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
-                        .antMatchers(HttpMethod.GET, "/funcionarios").hasRole("ADMIN")
-                        .antMatchers(HttpMethod.POST, "/funcionarios").hasRole("ADMIN")
-                        .antMatchers(HttpMethod.PUT, "/funcionarios/{id}").hasRole("ADMIN")
-                        .antMatchers(HttpMethod.DELETE, "/funcionarios/{id}").hasRole("ADMIN")
-                        .antMatchers(HttpMethod.GET, "/clientes").permitAll()
-                        .antMatchers(HttpMethod.POST, "/clientes").permitAll()
-                        .antMatchers(HttpMethod.PUT, "/clientes/{id}").permitAll()
-                        .antMatchers(HttpMethod.DELETE, "/clientes/{id}").permitAll()
-                        .antMatchers(HttpMethod.POST, "/os").permitAll()
-                        .antMatchers(HttpMethod.POST, "/usuarios").permitAll()
-                        .antMatchers(HttpMethod.GET, "/api/estado").permitAll()
-                        .antMatchers(HttpMethod.POST, "/api/estado").permitAll()
-                        .antMatchers(HttpMethod.PUT, "/api/estado/{id}").permitAll()
-                        .antMatchers(HttpMethod.DELETE, "/api/estado/{id}").permitAll()
-                        .antMatchers(HttpMethod.GET, "/api/cidade").permitAll()
-                        .antMatchers(HttpMethod.GET, "/api/cidade/{id}").permitAll()
-                        .antMatchers(HttpMethod.POST, "/api/cidade").permitAll()
-                        .antMatchers(HttpMethod.PUT, "/api/cidade/{id}").permitAll()
-                        .antMatchers(HttpMethod.DELETE, "/api/cidade/{id}").permitAll()
-                        .antMatchers(HttpMethod.GET, "/pessoas").permitAll()
-                        .antMatchers(HttpMethod.POST, "/pessoas").permitAll()
-                        .antMatchers(HttpMethod.PUT, "/pessoas/{id}").permitAll()
-                        .antMatchers(HttpMethod.DELETE, "/pessoas/{id}").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().permitAll() // Permitir todas as requisições sem autenticação
+                );
     }
 
     @Bean
@@ -77,15 +39,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
+
